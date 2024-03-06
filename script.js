3644fce233d4e09141b93829160e734475b8ea44
Kamran Kiani <kiani.km@gmail.com>
2024-03-06 17:04:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 "use strict";
 
+// MANUALLY VALIDATION
+// // Const variables
+// const formEl = document.querySelector(".offer__signup-form");
+
+// const inputEls = [...document.querySelectorAll(".signup-form__input")];
+
+// const errorMsgEls = [
+//   ...document.querySelectorAll(".signup-form__error-message"),
+// ];
+
+// const errorIconEls = [...document.querySelectorAll(".signup-form__error-icon")];
+
+// const buttonEl = document.querySelector(".signup-form__button");
+
+// function validateEmail(email) {
+//   const re =
+//     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
+
+// function changeStyle(errorMsgEl, errorIconEl, inputEl) {
+//   errorMsgEl.classList.remove("hidden");
+//   errorIconEl.classList.remove("hidden");
+//   inputEl.setAttribute("aria-invalid", "true");
+// }
+
+// formEl.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   for (let i = 0; i < inputEls.length; i++) {
+//     if (inputEls[i].value.trim() === "") {
+//       errorMsgEls[i].textContent = `${inputEls[i].name} cannot be empty`;
+//       changeStyle(errorMsgEls[i], errorIconEls[i], inputEls[i]);
+//     } else if (
+//       inputEls[i].type === "email" &&
+//       !validateEmail(inputEls[i].value)
+//     ) {
+//       errorMsgEls[i].textContent = "Looks like this is not an email";
+//       changeStyle(errorMsgEls[i], errorIconEls[i], inputEls[i]);
+//     } else {
+//       errorMsgEls[i].classList.add("hidden");
+//       errorIconEls[i].classList.add("hidden");
+//       inputEls[i].setAttribute("aria-invalid", "false");
+//     }
+//   }
+//   const allValid = inputEls.every(
+//     (inputEl) => inputEl.getAttribute("aria-invalid") === "false"
+//   );
+//   if (allValid) {
+//     e.target.submit();
+//   }
+// });
+// ----------------------------------------------------------------------------
+
+// CONSTRAINT API VALIDATION
 // Const variables
 const formEl = document.querySelector(".offer__signup-form");
 
@@ -11,14 +66,6 @@ const errorMsgEls = [
 
 const errorIconEls = [...document.querySelectorAll(".signup-form__error-icon")];
 
-const buttonEl = document.querySelector(".signup-form__button");
-
-function validateEmail(email) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
 function changeStyle(errorMsgEl, errorIconEl, inputEl) {
   errorMsgEl.classList.remove("hidden");
   errorIconEl.classList.remove("hidden");
@@ -29,31 +76,28 @@ formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
   for (let i = 0; i < inputEls.length; i++) {
-    if (inputEls[i].value.trim() === "") {
-      console.log(inputEls[i]);
-      errorMsgEls[i].textContent = `${inputEls[i].name} cannot be empty`;
-      changeStyle(errorMsgEls[i], errorIconEls[i], inputEls[i]);
-    } else if (
-      inputEls[i].type === "email" &&
-      !validateEmail(inputEls[i].value)
-    ) {
-      errorMsgEls[i].textContent = "Looks like this is not an email";
-      changeStyle(errorMsgEls[i], errorIconEls[i], inputEls[i]);
+    let inputEl = inputEls[i];
+    let errorMsgEl = errorMsgEls[i];
+    let errorIconEl = errorIconEls[i];
+
+    if (!inputEl.checkValidity()) {
+      if (inputEl.validity.valueMissing) {
+        errorMsgEl.textContent = `${inputEl.name} cannot be empty`;
+      } else if (inputEl.validity.typeMismatch) {
+        errorMsgEl.textContent = "Looks like this is not an email";
+      }
+      changeStyle(errorMsgEl, errorIconEl, inputEl);
     } else {
-      errorMsgEls[i].classList.add("hidden");
-      errorIconEls[i].classList.add("hidden");
-      inputEls[i].setAttribute("aria-invalid", "false");
+      errorMsgEl.classList.add("hidden");
+      errorIconEl.classList.add("hidden");
+      inputEl.setAttribute("aria-invalid", "false");
     }
   }
-  const allHaveHiddenClass = errorMsgEls.every((element) =>
-    element.classList.contains("hidden")
+
+  const allValid = inputEls.every(
+    (inputEl) => inputEl.getAttribute("aria-invalid") === "false"
   );
-  console.log(allHaveHiddenClass);
-  if (allHaveHiddenClass) {
+  if (allValid) {
     e.target.submit();
   }
 });
-
-// for (let i = 0; i < inputEls.length; i++) {
-//   console.log(inputEls[i]);
-// }
